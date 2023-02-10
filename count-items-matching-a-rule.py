ad1e1318b1ebba69f30c8807e29a6b6153b2177a
elpinkypie <46905531+elpinkypie@users.noreply.github.com>
2023-02-10 23:35:21 +0200

    Create count-items-matching-a-rule.py

---
+++ count-items-matching-a-rule.py
@@ -0,0 +1,27 @@
+"""https://leetcode.com/problems/count-items-matching-a-rule/
+Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+Output: 1
+"""
+
+from typing import List
+
+
+class Solution:
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        counter = 0
+        for item in items:
+            # for element in item:
+                if ruleKey == "type" and ruleValue == item[0]:
+                    counter = counter + 1
+                if ruleKey == "color" and ruleValue == item[1]:
+                    counter = counter + 1
+                if ruleKey == "name" and ruleValue == item[2]:
+                    counter = counter + 1
+
+        return counter
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.countMatches(
+        items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+    ))
