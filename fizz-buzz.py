110e6698b9e34c39c0b4aae0f910ee0c409f911a
elpinkypie <46905531+elpinkypie@users.noreply.github.com>
2024-12-15 19:20:49 +0200

    Update fizz-buzz.py

--- fizz-buzz.py
+++ fizz-buzz.py
@@ -22,3 +22,11 @@ class Solution:
 if __name__ == "__main__":
     solution = Solution()
     print(solution.fizzBuzz(n=3))
+
+
+# Here is another solution: 
+
+def fizzBuzz(n: int):
+    for i in range(1, n + 1):
+        result = ("Fizz" * (i % 3 == 0)) + ("Buzz" * (i % 5 == 0))
+        print(result or i)
